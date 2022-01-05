88212189c7c73394cff75b42529f40d9065e58e7
michonchy <mintan0222@gmail.com>
2022-01-05 11:32:11 +0900

    ドル換算

--- currency_conversion/app.py
+++ currency_conversion/app.py
@@ -28,10 +28,9 @@ def split_numbers(text: str):
     return number_list
 
 def is_currency_conversion(numbers:List[int])->str:
-    d = numbers[0]/numbers[1]
-    d = int(d)
-    s = (d*100)/numbers[1]
-    s = int(s)
+    d = numbers[0]//numbers[1]
+    s = numbers[0]%numbers[1]
+    s = s*100//numbers[1]
     return d,s
 
 
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -7,3 +7,6 @@ from currency_conversion import app
 
 def test_split_numbers():
     assert app.split_numbers("1,2,3") == [1,2,3]
+
+def test_is_currency_conversion():
+    assert app.is_currency_conversion([10,3]) == (3,33)
